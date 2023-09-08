b198392cec5c53cd3ec3529a3011b18b8d444d55
DESKTOP-EIVOOOA\ASUS <qkrwnsdh00@gmail.com>
2023-09-08 15:28:51 +0900

    feat normalZombie Attack Ani, Add Layer Left, Right, Body, Head Attack, Add AniController

---
+++ KillingFloor/Assets/00_KillingFloor/Scripts/NormalZombies/NormalZombieSpawner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalZombieSpawner : MonoBehaviour
+{
+    public List<GameObject> zombieStyle = new List<GameObject>();
+
+    public List<GameObject> zombieList = new List<GameObject>();
+
+    //public void SpawnZombie(int count, Vector3 point)
+    //{
+    //    for (int i = 0; i <= zombieList.Count; i++)
+    //    {
+    //        if (zombieList.Count == i)
+    //        { 
+    //        zombieList.Add(Instantiate())
+    //        }
+    //    }
+    //}
+}
