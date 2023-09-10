a43d4f8cf98b167c3950c0b96b62c31af6f42c67
DESKTOP-EIVOOOA\ASUS <qkrwnsdh00@gmail.com>
2023-09-10 16:40:19 +0900

    style format, refactor NormalZombie or Data, feat Skill ani, Code

--- KillingFloor/Assets/00_KillingFloor/Scripts/NormalZombies/AttackHand.cs
+++ KillingFloor/Assets/00_KillingFloor/Scripts/NormalZombies/AttackHand.cs
@@ -4,14 +4,7 @@ using UnityEngine;
 
 public class AttackHand : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
         
     }
--- KillingFloor/Assets/00_KillingFloor/Scripts/NormalZombies/NormalZombie.cs
+++ KillingFloor/Assets/00_KillingFloor/Scripts/NormalZombies/NormalZombie.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using Unity.VisualScripting;
 using UnityEditor.Animations;
 using UnityEditor.Experimental.GraphView;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Rendering.Universal;
 
 public class NormalZombie : NormalZombieData
 {
     public List<AnimatorController> controllers = new List<AnimatorController>();
 
-    private Coroutine aniCoroutine;
+    private Coroutine blendTreeMove;
+    private Coroutine animatorController;
 
     private NormalNavigation navigation;
 
@@ -21,7 +24,7 @@ public class NormalZombie : NormalZombieData
 
     // 현재 애니메이션 좌표
     private float thisBlend;
-
+    private float coolTime;
     public int hitPos;
 
     /*
@@ -33,25 +36,30 @@ public class NormalZombie : NormalZombieData
 
     public bool isCoroutine = false;
     private bool isDeath = false;
+    private bool isHit = false;
+    private bool isSkill = false;
 
     private void Awake()
     {
         navigation = GetComponent<NormalNavigation>();
         ani = GetComponent<Animator>();
-
+        GetComponent<NavMeshAgent>().speed = 0.1f;
+        if (gameObject.name == "ZombieWalk_01" || gameObject.name == "ZombieWalk_02" ||
+            gameObject.name == "ZombieWalk_03" || gameObject.name == "ZombieWalk_04" ||
+            gameObject.name == "ZombieHide_01" || gameObject.name == "ZombieHide_01")
+        {
+            isSkill = true;
+        }
+        else { /*No Event*/ }
     }
 
     private void OnEnable()
     {
         ZombieSetting();
-        ani.runtimeAnimatorController = controllers[Random.Range(0, controllers.Count)];
-        aniCoroutine = StartCoroutine(AnimationCoroutine(0.0f, 1.0f, 2.0f, true, "null"));
     }
     private void Start()
     {
         ZombieSetting();
-        ani.runtimeAnimatorController = controllers[Random.Range(0, controllers.Count)];
-        aniCoroutine = StartCoroutine(AnimationCoroutine(0.0f, 1.0f, 2.0f, true, "null"));
     }
 
     private void Update()
@@ -60,127 +68,163 @@ public class NormalZombie : NormalZombieData
         {
             if (health <= 0)
             {
-                StopCoroutine(aniCoroutine);
-                StartCoroutine(AnimationCoroutine(0.0f, 1.0f, 2.0f, true, "null"));
                 Death();
             }
+            if (health == health / 2 && isHit == false)
+            {
+                isHit = true;
 
+                Hit(hitPos);
+            }
             if (isCoroutine == false)
             {
-                if (navigation.isContact == true)
+                if (navigation.isLongContact == true && isSkill == false)
                 {
-                    ani.SetBool("isAttack", true);
-
+                    Skill();
+                }
+                else if (navigation.isContact == true)
+                {
                     Attack();
                 }
-                else
-                {
-                    ani.SetBool("isAttack", false);
-                }
+                else { /*No Event*/ }
             }
         }
     }
 
     private void ZombieSetting()
     {
-        int number = Random.Range(0, 5);
+        ani.runtimeAnimatorController = controllers[Random.Range(0, controllers.Count)];
+        blendTreeMove = StartCoroutine(BlendTreeMove(0.0f, 1.0f, 2.0f));
 
-        switch (number)
+        if (gameObject.name == "ZombieWalk_01" || gameObject.name == "ZombieWalk_02" ||
+            gameObject.name == "ZombieWalk_03" || gameObject.name == "ZombieWalk_04")
         {
-            case 0:
-                (health, damage, coin) = ZombieWalk();
-                break;
-            case 1:
-                (health, damage, coin) = ZombieRun();
-                break;
-            case 2:
-                (health, damage, coin) = ZombieSpit();
-                break;
-            case 3:
-                (health, damage, coin) = ZombieHide();
-                break;
-            case 4:
-                (health, damage, coin) = ZombieNoise();
-                break;
-        }
+            int number = Random.Range(0, 2);
 
-        GetComponent<NavMeshAgent>().speed = 0.1f;
+            switch (number)
+            {
+                case 0:
+                    (health, damage, coin) = ZombieWalk();
+                    break;
+                case 1:
+                    (health, damage, coin) = ZombieRun();
+                    break;
+            }
+        }
+        else if (gameObject.name == "ZombieHide_01" || gameObject.name == "ZombieHide_01")
+        {
+            (health, damage, coin) = ZombieHide();
+        }
+        else if (gameObject.name == "ZombieSpit_01" || gameObject.name == "ZombieSpit_02")
+        {
+            (health, damage, coin) = ZombieSpit();
+        }
+        else if (gameObject.name == "ZombieNoise")
+        {
+            (health, damage, coin) = ZombieNoise();
+        }
     }
 
-    private IEnumerator AnimationCoroutine(float startBlend, float endBlend, float duration, bool isBlend, string checkName)
+    private IEnumerator BlendTreeMove(float startBlend, float endBlend, float duration)
     {
         isCoroutine = true;
 
-        if (isBlend)
+        if (!(thisBlend == endBlend))
         {
-            if (!(thisBlend == endBlend))
+            timeElapsed = 0.0f;
+
+            thisBlend = ani.GetFloat("move");
+
+            while (timeElapsed < duration)
             {
-                timeElapsed = 0.0f;
+                timeElapsed += Time.deltaTime;
 
-                thisBlend = ani.GetFloat("move");
+                float time = Mathf.Clamp01(timeElapsed / duration);
 
-                while (timeElapsed < duration)
-                {
-                    timeElapsed += Time.deltaTime;
+                ani.SetFloat("move", Mathf.Lerp(startBlend, endBlend, time));
 
-                    float time = Mathf.Clamp01(timeElapsed / duration);
-
-                    ani.SetFloat("move", Mathf.Lerp(startBlend, endBlend, time));
-
-                    yield return null;
-                }
+                yield return null;
             }
-        }
-        else
-        {
-            ani.SetBool(checkName, true);
-
-            float clipTime = ani.GetCurrentAnimatorClipInfo(0).Length;
-
-            yield return new WaitForSeconds(clipTime);
-
-            ani.SetBool(checkName, false);
         }
 
         isCoroutine = false;
     }
 
+    private IEnumerator AnimatorController(string checkName)
+    {
+        isCoroutine = true;
+        ani.SetBool(checkName, true);
+
+        yield return new WaitForSeconds(ani.GetCurrentAnimatorStateInfo(0).length);
+
+        ani.SetBool(checkName, false);
+        isCoroutine = false;
+    }
+
+    private IEnumerator CoolTime(float time)
+    {
+        isSkill = true;
+
+        coolTime = 0.0f;
+
+        while (coolTime <= time)
+        {
+            coolTime += Time.deltaTime;
+
+            yield return null;
+        }
+
+        isSkill = false;
+    }
+
     private void Attack()
     {
-        ani.SetTrigger("isChange");
-
         int number = Random.Range(0, 2);
 
         switch (number)
         {
             case 0:
-                StartCoroutine(AnimationCoroutine(0.0f, 0.0f, 0.0f, false, "atk01"));
+                animatorController = StartCoroutine(AnimatorController("atk01"));
                 break;
             case 1:
-                StartCoroutine(AnimationCoroutine(0.0f, 0.0f, 0.0f, false, "atk02"));
+                animatorController = StartCoroutine(AnimatorController("atk02"));
                 break;
         }
     }
 
     public void Hit(int number)
     {
-        ani.SetTrigger("isChange");
+        if (blendTreeMove != null) { StopCoroutine(blendTreeMove); }
+        if (animatorController != null) { StopCoroutine(animatorController); }
 
         switch (number)
         {
-            case 0:
-                StartCoroutine(AnimationCoroutine(0.0f, 0.0f, 0.0f, false, "hitHead"));
+            case 0: // 왼쪽
+                ani.SetFloat("hitPosX", 0.0f);
+                ani.SetFloat("hitPosY", -1.0f);
                 break;
-            case 1:
-                StartCoroutine(AnimationCoroutine(0.0f, 0.0f, 0.0f, false, "hitLeft"));
+            case 1: // 정면
+                ani.SetFloat("hitPosX", -1.0f);
+                ani.SetFloat("hitPosY", 0.0f);
                 break;
-            case 2:
-                StartCoroutine(AnimationCoroutine(0.0f, 0.0f, 0.0f, false, "hitFront"));
+            case 2: // 오른쪽
+                ani.SetFloat("hitPosX", 0.0f);
+                ani.SetFloat("hitPosY", 1.0f);
                 break;
-            case 3:
-                StartCoroutine(AnimationCoroutine(0.0f, 0.0f, 0.0f, false, "hitRight"));
+            case 3: // 머리
+                ani.SetFloat("hitPosX", 1.0f);
+                ani.SetFloat("hitPosY", 0.0f);
                 break;
         }
+
+        animatorController = StartCoroutine(AnimatorController("isHit"));
+    }
+
+    public void Skill()
+    {
+        StartCoroutine(CoolTime(10.0f));
+
+        animatorController = StartCoroutine(AnimatorController("atkScreaming"));
     }
 
     private void Death()
@@ -214,4 +258,43 @@ public class NormalZombie : NormalZombieData
     {
         return base.ZombieNoise(health, damage, coin);
     }
+
+    //Legacy:
+    //private IEnumerator AnimationCoroutine(float startBlend, float endBlend, float duration, bool isBlend, string checkName)
+    //{
+    //    isCoroutine = true;
+
+    //    if (isBlend)
+    //    {
+    //        if (!(thisBlend == endBlend))
+    //        {
+    //            timeElapsed = 0.0f;
+
+    //            thisBlend = ani.GetFloat("move");
+
+    //            while (timeElapsed < duration)
+    //            {
+    //                timeElapsed += Time.deltaTime;
+
+    //                float time = Mathf.Clamp01(timeElapsed / duration);
+
+    //                ani.SetFloat("move", Mathf.Lerp(startBlend, endBlend, time));
+
+    //                yield return null;
+    //            }
+    //        }
+    //    }
+    //    else
+    //    {
+    //        ani.SetBool(checkName, true);
+
+    //        float clipTime = ani.GetCurrentAnimatorClipInfo(0).Length;
+
+    //        yield return new WaitForSeconds(clipTime);
+
+    //        ani.SetBool(checkName, false);
+    //    }
+
+    //    isCoroutine = false;
+    //}
 }
