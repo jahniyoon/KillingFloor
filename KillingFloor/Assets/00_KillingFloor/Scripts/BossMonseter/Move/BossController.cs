d8332560c0cd4d7937f7da0ba8012edffa2400cc
DESKTOP-2UEK7CH\KGA <a01049469494@gmail.com>
2023-09-13 16:06:27 +0900

    보스행동 효율화 중간push

--- KillingFloor/Assets/00_KillingFloor/Scripts/BossMonseter/Move/BossController.cs
+++ KillingFloor/Assets/00_KillingFloor/Scripts/BossMonseter/Move/BossController.cs
@@ -38,8 +38,8 @@ public class BossController : MonoBehaviour
 
         GameObject midSphere = GameObject.Find("MidSphereEffect");
         GameObject fireBreath = GameObject.Find("FireBreath");
-        GameObject fireBreathHole = GameObject.Find("FireBreathHole"); 
-        GameObject fireRing = GameObject.Find("FireRing"); 
+        GameObject fireBreathHole = GameObject.Find("FireBreathHole");
+        GameObject fireRing = GameObject.Find("FireRing");
 
         for (int i = 0; i <= 2; i++) // 점프 충격 배열에 저장하는과정
         {
@@ -60,16 +60,17 @@ public class BossController : MonoBehaviour
             midSphereEffects[i].SetActive(false);
         }
 
-        for (int i=0; i<=3; i++) // 브레스 파티클배열에 저장하는과정
+        for (int i = 0; i <= 3; i++) // 브레스 파티클배열에 저장하는과정
         {
             fireBreaths[i] = fireBreath.transform.GetChild(i).gameObject;
             fireBreathsParticle[i] = fireBreaths[i].GetComponent<ParticleSystem>();
             fireBreaths[i].SetActive(false);
         }
-        
+
         animator = GetComponent<Animator>();
         targetPlayer = GameObject.FindGameObjectsWithTag("Player");
         randPlayerNum = Random.Range(0, targetPlayer.Length);
+      
     }
 
     // Update is called once per frame
@@ -78,21 +79,28 @@ public class BossController : MonoBehaviour
         currentTime += Time.deltaTime;
         if (currentTime >= setTime)
         {
-            animator.SetTrigger("Idle");
 
+
+        
             // 플레이어와 보스 사이의 거리를 계산합니다.
             float distance = Vector3.Distance(targetPlayer[randPlayerNum].transform.position, transform.position);
-            speed = (distance/2)*4;
+          
             if (distance < 6f)//근거리 애니메이션
-            {   
-                randomFattern = Random.Range(0, 4);
-                if(randomFattern != saveFattern)// 공격패턴 중복체크, 중복패턴 변경
+            {
+                agent.isStopped = true;
+                agent.updatePosition = false;
+                agent.updateRotation = false;
+                agent.velocity = Vector3.zero;
+
+                speed = 0;
+                randomFattern = Random.Range(0, 6);
+                if (randomFattern != saveFattern)// 공격패턴 중복체크, 중복패턴 변경
                 {
                     saveFattern = randomFattern;
                 }
-                else if(randomFattern == saveFattern)
+                else if (randomFattern == saveFattern)
                 {
-                    if(randomFattern !=0)
+                    if (randomFattern != 0)
                     {
                         randomFattern = 0;
                         saveFattern = 0;
@@ -107,73 +115,86 @@ public class BossController : MonoBehaviour
                 switch (randomFattern)//보스 공격패턴
                 {
                     case 0:
-                     
+
                         animator.SetTrigger("Attack1");//찍기
                         currentTime = 0;
+                    
                         setTime = 3.7f;
+                      
                         break;
                     case 1:
-                     
+
                         animator.SetTrigger("Attack2");//가르기
                         currentTime = 0;
+                     
                         setTime = 3.7f;
                         break;
                     case 2:
-                     
+
                         animator.SetTrigger("Attack3");//브레스
-                        for(int i =0; i <= 2; i++)
-                        {
-                            fireBreaths[i].SetActive(true);
-                            fireBreathsParticle[i].Play();
-                        }
-                        currentTime = 0;
-                        setTime = 5.75f;
-                        break;
-                    case 3:
-                    
-                        animator.SetTrigger("Shout");//짖기
-                        for (int i = 0; i <= 1; i++)
-                        {
-                            fireBreathHoles[i].SetActive(true);
-                            fireBreathHoleParticles[i].Play();
-                            midSphereEffects[i].SetActive(true);
-                            if(i == 1)
-                            {
-                                midSphereEffectParticles[i].Play();
-                                StartCoroutine(StopParticle(midSphereEffectParticles[i], 3.6f));//파티클 정지 코루틴
-                                StartCoroutine(FalseObj(midSphereEffects[0], 3.6f));//오브젝트 정지 코루틴
-                            }                       
-                            StartCoroutine(StopParticle(fireBreathHoleParticles[i], 3.6f));//파티클 정지 코루틴
-                          
-
-                        }
-
-                        currentTime = 0;
-                        setTime = 3.75f;
-                        break;
-                    case 4:
-                   
-                        animator.SetTrigger("Jump");
-                        Invoke("JumpImpt", 1.5f);
-                        currentTime = 0;
-                        setTime = 2;
-                        break;
-                    case 5:
                         for (int i = 0; i <= 2; i++)
                         {
                             fireBreaths[i].SetActive(true);
                             fireBreathsParticle[i].Play();
                         }
                         currentTime = 0;
+                       
                         setTime = 5.75f;
+                        break;
+                    case 3:
+
+                        animator.SetTrigger("Shout");//짖기
+                        for (int i = 0; i <= 1; i++)
+                        {
+                            fireBreathHoles[i].SetActive(true);
+                            fireBreathHoleParticles[i].Play();
+                            midSphereEffects[i].SetActive(true);
+                            if (i == 1)
+                            {
+                                midSphereEffectParticles[i].Play();
+                                StartCoroutine(StopParticle(midSphereEffectParticles[i], 3.6f));//파티클 정지 코루틴
+                                StartCoroutine(FalseObj(midSphereEffects[0], 3.6f));//오브젝트 정지 코루틴
+                            }
+                            StartCoroutine(StopParticle(fireBreathHoleParticles[i], 3.6f));//파티클 정지 코루틴
+
+
+                        }
+                        
+                        currentTime = 0;
+                        setTime = 3.6f;
+                        break;
+                    case 4:
+                        animator.SetTrigger("Jump");
+                        Invoke("JumpImpt", 1.1f);
+                        currentTime = 0;
+                       
+                        setTime = 3f;
+                        break;
+                    case 5:
+                        animator.SetTrigger("Breath");
+                        Invoke("fireBreathImpt", 1f);
+                        currentTime = 0;
+                       
+                        setTime = 5.8f;
                         break;
                 }
             }//보스 공격패턴End
             else//원거리 애니메이션
-            {                            
-                animator.SetFloat("Speed", speed);
-                LookRotate();
-                BossMove();
+            {
+            
+                AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+                if (stateInfo.IsName("idle"))// 속도 변화
+                {
+                    agent.isStopped = false;
+                    agent.updatePosition = true;
+                    agent.updateRotation = true;
+                    speed = (distance / 2) * 4;
+                 
+                   // LookRotate();
+                    BossMove();
+                }
+                animator.SetFloat("Speed", agent.speed);
+
 
 
             }//원거리 애니메이션 end
@@ -181,7 +202,7 @@ public class BossController : MonoBehaviour
         }
 
     }
-    //대상을 바라보는 로직 
+    //대상을 바라보는 로직 (사용안함)
     private void LookRotate()
     {
         if (targetPlayer[randPlayerNum] == null) // 대상사라질경우
@@ -202,23 +223,25 @@ public class BossController : MonoBehaviour
     //보스 움직임
     private void BossMove()
     {
-      if(targetPlayer[randPlayerNum] != null)
+        if (targetPlayer[randPlayerNum] != null)
         {
             agent.destination = targetPlayer[randPlayerNum].transform.position;
         }
-     
+
     }
     //파티클 정지
     private IEnumerator StopParticle(ParticleSystem particle, float num)
     {
         yield return new WaitForSeconds(num);
         particle.Stop();
+ 
     }
     //오브젝트 비활성화
     private IEnumerator FalseObj(GameObject gameObj, float num)
     {
         yield return new WaitForSeconds(num);
         gameObj.SetActive(false);
+
     }
     //점프 충격
     private void JumpImpt()
@@ -229,7 +252,16 @@ public class BossController : MonoBehaviour
             fireRingParticles[i].Play();
             StartCoroutine(StopParticle(fireRingParticles[i], 1f));
         }
-        
+
     }
-   
+    private void fireBreathImpt()
+    {
+        for (int i = 0; i <= 2; i++)
+        {
+            fireBreaths[i].SetActive(true);
+            fireBreathsParticle[i].Play();
+            StartCoroutine(StopParticle(fireBreathsParticle[i], 4f));
+        }
+    }
+ 
 }
