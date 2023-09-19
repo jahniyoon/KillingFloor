90c61f022bf44b6cf31362f3e3f6e6e0080acfba
DESKTOP-EIVOOOA\ASUS <qkrwnsdh00@gmail.com>
2023-09-19 13:34:18 +0900

    Add player package

--- KillingFloor/Assets/00_KillingFloor/Scripts/Global/GameManager.cs
+++ KillingFloor/Assets/00_KillingFloor/Scripts/Global/GameManager.cs
@@ -76,7 +76,7 @@ public class GameManager : MonoBehaviourPunCallbacks, IPunObservable
     void Start()
     {
         // 생성할 랜덤 위치 지정
-        Vector3 spawnPosition = new Vector3 (8f,0f,8f);
+        Vector3 spawnPosition = new Vector3 (0f,1f,0f);
         // 네트워크 상의 모든 클라이언트들에서 생성 실행
         // 단, 해당 게임 오브젝트의 주도권은, 생성 메서드를 직접 실행한 클라이언트에게 있음
         PhotonNetwork.Instantiate(playerPrefab.name, spawnPosition, Quaternion.identity);
